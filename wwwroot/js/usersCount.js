5e4380e57308e07b76c3fa098c7945bb0f3c4c20
Bala Krishnan Poominathan <bpoominathan@worldbankgroup.org>
2025-01-27 17:07:06 +0530

    POC

---
+++ wwwroot/js/usersCount.js
@@ -0,0 +1,37 @@
+﻿//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder()
+    //.configureLogging(signalR.LogLevel.Information)
+    .withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build()
+//.configureLogging(signalR.LogLevel.Information)
+//.withAutomaticReconnect()
+//.withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build();
+
+////connect to methods that hub invokes aka receive notfications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    console.log(value);
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.invoke("NewWindowLoaded","test").then((value) => console.log(value));
+    // connectionUserCount.invoke("NewWindowLoaded", "Bhrugen").then((value) => console.log(value));
+}
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+function rejected() {
+    //rejected logs
+}
+connectionUserCount.start().then(fulfilled, rejected);
